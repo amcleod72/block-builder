ec69c683343404c1024f7140f1d87c8f2ae37bc0
amcleod72 <andrew@mcleods.net>
2019-07-24 11:26:41 +1000

    make it better

--- app.js
+++ app.js
@@ -3,12 +3,11 @@ var express         = require('express');
 var path            = require('path');
 var http            = require('http');
 var app             = express();
-var expressLogging  = require('express-logging');
-var logger          = require('logops');
+var morgan          = require('morgan');
 
+app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname, 'Public')));
 app.set('port', process.env.PORT || 4000);
-app.use(expressLogging(logger));
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
