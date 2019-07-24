cf731d2053ae42da37a880ff1ffeb121a7e61eff
amcleod72 <andrew@mcleods.net>
2019-07-24 11:47:26 +1000

    make it better

--- app.js
+++ app.js
@@ -3,11 +3,17 @@ var express         = require('express');
 var path            = require('path');
 var http            = require('http');
 var app             = express();
-var morgan          = require('morgan');
+var audit = require('express-requests-logger');
 
-app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname, 'Public')));
 app.set('port', process.env.PORT || 4000);
+
+app.use(audit({
+    logger: logger, // Existing bunyan logger
+    excludeURLs: [‘health’, ‘metrics’], // Exclude paths which enclude 'health' & 'metrics'
+    request: {},
+    response: {}
+}));
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
