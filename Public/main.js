3cc681375a6d13565b8e9f06701f3a8d30cd017e
amcleod72 <andrew@mcleods.net>
2019-08-03 14:37:37 +1000

    make it better

--- Public/main.js
+++ Public/main.js
@@ -57,7 +57,6 @@ $('document').ready(function() {
 
     function getRecord(primaryKeyField,primaryKey){
         var endpoint = window.location.protocol + "//" + window.location.hostname + ":" + window.location.port;
-        endpoint += "/data" + deKey;
         endpoint += "?" + primaryKeyField;
         endpoint += "=" + primaryKey;
 
